e66e3c1deda2912acde9c31195c2329b3fc78aca
Marcos Puente Blanco <marcosfppf@gmail.com>
2021-01-11 17:22:34 +0100

    borrada carpeta springboot_hibernate

--- springboot_hibernate/src/main/java/des/springboot_hibernate/dao/EmailDao.java
+++
@@ -1,7 +0,0 @@
-package des.springboot_hibernate.dao;
-
-import des.springboot_hibernate.entidades.Email;
-
-public interface EmailDao extends DaoGenerico<Email> {
-
-}
--- springboot_hibernate/src/main/java/des/springboot_hibernate/dao/ImagenDao.java
+++
@@ -1,9 +0,0 @@
-package des.springboot_hibernate.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import des.springboot_hibernate.entidades.Imagen;
-
-public interface ImagenDao extends CrudRepository<Imagen, Long> {
-
-}
