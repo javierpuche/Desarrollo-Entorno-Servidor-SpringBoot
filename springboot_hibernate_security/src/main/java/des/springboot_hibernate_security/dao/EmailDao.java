0455acd36c0205bcdf13343f76bcbec31b35de7f
Marcos Puente Blanco <marcosfppf@gmail.com>
2021-01-11 17:23:37 +0100

    borrada carpeta springboot_hibernate_security

--- springboot_hibernate_security/src/main/java/des/springboot_hibernate_security/dao/EmailDao.java
+++
@@ -1,7 +0,0 @@
-package des.springboot_hibernate_security.dao;
-
-import des.springboot_hibernate_security.entidades.Email;
-
-public interface EmailDao extends DaoGenerico<Email> {
-
-}
--- springboot_hibernate_security/src/main/java/des/springboot_hibernate_security/seguridad/SecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package des.springboot_hibernate_security.seguridad;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
